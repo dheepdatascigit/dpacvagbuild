cdbc3998089a4ed9b727b70d1e1dc5b32233c169
Dheep net-research <dheep@net-research.com.au>
2018-03-22 00:05:55 +1100

    add gns3 build script

---
+++ vagrant/vag_scripts/gns3.sh
@@ -0,0 +1,15 @@
+
+sudo apt-get -y install python3-setuptools
+sudo apt-get -y install python3-aiohttp
+sudo apt-get -y install python3-psutil
+sudo apt-get -y install python3-jsonschema
+
+https://github.com/GNS3/gns3-server.git
+
+cd gns3-server
+sudo python3 setup.py install
+
+# gns3server
+# To run tests use:
+
+# py.test -v
