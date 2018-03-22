b910d548a370b1bea6c283979a95676cd3e9262e
Dheep net-research <dheep@net-research.com.au>
2018-03-22 22:08:14 +1100

    gns3 script and fix ho my zsh install

--- vagrant/vag_scripts/gns3.sh
+++ vagrant/vag_scripts/gns3.sh
@@ -4,7 +4,7 @@ sudo apt-get -y install python3-aiohttp
 sudo apt-get -y install python3-psutil
 sudo apt-get -y install python3-jsonschema
 
-https://github.com/GNS3/gns3-server.git
+git clone https://github.com/GNS3/gns3-server.git
 
 cd gns3-server
 sudo python3 setup.py install
--- vagrant/vag_scripts/initshell.sh
+++ vagrant/vag_scripts/initshell.sh
@@ -11,4 +11,4 @@ sudo apt-get update
 sudo apt-get -y install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
-sudo apt-get -y install ansible
+sudo apt-get -y install ansible git git-core
